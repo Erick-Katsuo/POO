947c1ffc99088bef512a0e29c51f682d32005b54
Katsuo <49467285+Erick-Katsuo@users.noreply.github.com>
2020-08-18 12:11:38 -0300

    Add files via upload

---
+++ C++/Cabeceras-Headers/saludo.hpp
@@ -0,0 +1,17 @@
+#ifndef __SALUDO_HPP__
+#define __SALUDO_HPP__
+
+#include <iostream>
+using namespace std;
+
+/*Pre:-.
+ *Post: Imprime un saludo por consola.
+*/
+void saludar(); // declarar el procediiento "saludar"
+
+
+
+
+
+
+#endif /*__SALUDO_HPP__*/
