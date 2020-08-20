a83bb8a6f0dc555eac79f34f017f6c83a41fd303
Katsuo <49467285+Erick-Katsuo@users.noreply.github.com>
2020-08-20 11:15:03 -0300

    Add files via upload

---
+++ C++/Cabeceras-Headers/Ej1 header - v2/ej2_struct - v2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+/*
+	- Crear un tipo de dato persona con atributos: dni, inicial_nombre, edad, si tiene gato, si tiene perro
+	- Una vez creado el tipo de dato persona proceda a definirlo en la funcion main 
+	- Luego asignar valores ficticios a esta variable/struct de las siguientes maneras:
+		- Junto a la definicion
+		- Igualandolo a otra variable/struct
+		- De campo a campo, o en otras palabras de atributo a atributo
+*/
+using namespace std;
+
+#include "matematica.hpp"
+
+int main(){
+	iniciar_programa();
+	return 0;
+}
