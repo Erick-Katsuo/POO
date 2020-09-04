e0879091312d39f1235281d87eb2f6f8030d6b6f
Katsuo <49467285+Erick-Katsuo@users.noreply.github.com>
2020-09-04 12:31:48 -0300

    Add files via upload

--- C++/TP ta te ti/juego.cpp
+++ C++/TP ta te ti/juego.cpp
@@ -5,34 +5,63 @@ using namespace std;
 const int FILA = 3;
 const int COLUMNA = 3;
 
+const int SIN_COMENZAR = -1;
+const int ACTIVO = 1;
+const int TERMINO = 0;
+
 int main(){
 
-	char tablero[FILA][COLUMNA] = {{'a' ,'b' ,'c'},
-								   {'d' ,'e' ,'f'},
-								   {'g', 'h', 'i'}};
+	char tablero[FILA][COLUMNA] = {};
+	int fila_ingresada = -1;
+	int columna_ingresada = -1;
+	char signo = '-';
+
+	int estado_juego = SIN_COMENZAR;
 
 
+	do{
+		cout<<"Indique coordenadas\nFila: ";
+		cin>> fila_ingresada;
+		cout<<"Columna: ";
+		cin>> columna_ingresada;
+		cout<<endl;
 
-	for(int fila_actual = 0; fila_actual < FILA ; fila_actual ++ ){
+		cout<<"Ingrese el signo a ingresar"<<endl;
+		cin>>signo;
 
-		for (int columna_actual = 0; columna_actual < COLUMNA; columna_actual++){
-			
-			cout << "Fila: " << fila_actual << " Columna: " << columna_actual << endl;
+		tablero[fila_ingresada][columna_ingresada] = signo;
 
+
+		for(int fila_actual = 0; fila_actual < FILA ; fila_actual ++ ){
+
+			for (int columna_actual = 0; columna_actual < COLUMNA; columna_actual++){
+				
+				cout<< "\t["<< tablero[fila_actual][columna_actual] << "]  ";
+
+			}
+			cout<<endl<<endl<<endl;
 		}
-	}
 
-	cout<<endl<<endl;
-
-	for(int fila_actual = 0; fila_actual < FILA ; fila_actual ++ ){
-
-		for (int columna_actual = 0; columna_actual < COLUMNA; columna_actual++){
-			
-			cout<< "\t("<< tablero[fila_actual][columna_actual] << ")  ";
-
+		//verica si gano el jugador X en las lineas horizontales
+		for(int fila_actual = 0; fila_actual < FILA ; fila_actual ++ ){
+			if((tablero[fila_actual][0] =='X') && (tablero[fila_actual][1] =='X') && (tablero[fila_actual][2] =='X')){
+				cout<<"Gano el jugador X"<<endl;
+				estado_juego = TERMINO;
+			}
 		}
-		cout<<endl<<endl<<endl;
-	}
+
+		//verica si gano el jugador O en las lineas horizontales
+		for(int fila_actual = 0; fila_actual < FILA ; fila_actual ++ ){
+			if((tablero[fila_actual][0] =='O') && (tablero[fila_actual][1] =='O') && (tablero[fila_actual][2] =='O')){
+				cout<<"Gano el jugador O"<<endl;
+				estado_juego = TERMINO;
+			}
+		}
+
+
+	}while(estado_juego != TERMINO);
+
+
 
 	return 0;
 }
