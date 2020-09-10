4ed723e11e4f39cd92b138e94eff9f196cf92117
Katsuo <49467285+Erick-Katsuo@users.noreply.github.com>
2020-09-10 12:40:51 -0300

    Add files via upload

--- C++/TP ta te ti/juego.cpp
+++ C++/TP ta te ti/juego.cpp
@@ -9,9 +9,118 @@ const int SIN_COMENZAR = -1;
 const int ACTIVO = 1;
 const int TERMINO = 0;
 
+
+
+
+/*Pre: "tablero" actualizado y "signo" correspondiente al turno
+ *Post: retorna "estado_juego" actualizado
+*/
+int verificar_horizontal(char tablero[][COLUMNA], char signo){
+	int estado_juego = ACTIVO;
+
+	for(int fila_actual = 0; fila_actual < FILA ; fila_actual ++ ){
+		if((tablero[fila_actual][0] == signo) && (tablero[fila_actual][1] == signo) && (tablero[fila_actual][2] == signo)){
+			cout<<"Gano el jugador "<< signo <<endl;
+			estado_juego = TERMINO;
+		}
+	}
+
+	return estado_juego;
+}
+
+/*Pre: "tablero" actualizado y "signo" correspondiente al turno
+ *Post: retorna "estado_juego" actualizado
+*/
+int verificar_vertical(char tablero[][COLUMNA], char signo){
+	int estado_juego = ACTIVO;
+
+	for(int columna_actual = 0; columna_actual < COLUMNA ; columna_actual ++ ){
+		if((tablero[0][columna_actual] == signo) && (tablero[1][columna_actual] == signo) && (tablero[2][columna_actual] == signo)){
+			cout<<"Gano el jugador "<< signo <<endl;
+			estado_juego = TERMINO;
+		}
+	}
+
+	return estado_juego;
+}
+
+/*Pre: "tablero" actualizado y "signo" correspondiente al turno
+ *Post: retorna "estado_juego" actualizado
+*/
+int verificar_diagonal(char tablero[][COLUMNA], char signo){
+	int estado_juego = ACTIVO;
+
+	if((tablero[2][0] == signo) && (tablero[1][1] == signo) && (tablero[0][2] == signo)){
+		cout<<"Gano el jugador "<< signo <<endl;
+		estado_juego = TERMINO;
+	}else if((tablero[0][0] == signo) && (tablero[1][1] == signo) && (tablero[2][2] == signo)){
+		cout<<"Gano el jugador "<< signo <<endl;
+		estado_juego = TERMINO;
+	}
+
+	return estado_juego;
+}
+
+/*Pre: "tablero" actualizado y "signo" correspondiente al turno
+ *Post: retorna "estado_juego" actualizado
+*/
+int verificar_gano(char tablero[][COLUMNA], char signo){
+	int estado_juego = ACTIVO;
+
+	for(int i = 1 ; i < 4 ; i++){
+		switch(i){
+			case 1:
+				cout << "Verificando horizontal" <<endl;
+				estado_juego = verificar_horizontal(tablero, signo);
+				break;
+			case 2:
+				cout << "Verificando vertical" <<endl;
+				estado_juego = verificar_vertical(tablero, signo);
+				break;
+			case 3:
+				cout << "Verificando diagonal" <<endl;
+				estado_juego = verificar_diagonal(tablero, signo);
+		}
+
+		if(estado_juego == TERMINO){
+			i = 4;
+		}
+	}
+
+	return estado_juego;
+}
+
+
+int verificar_gano_jugador(char tablero[][COLUMNA]){
+	int estado_juego = ACTIVO;
+
+	cout<<"verificando jugador X"<<endl;
+	estado_juego = verificar_gano(tablero, 'X');
+	if(estado_juego != TERMINO){
+		cout<<"verificando jugador O"<<endl;
+		estado_juego = verificar_gano(tablero, 'O');
+	}
+
+	/*for(int i = 1 ; i < 3 ; i++){
+		switch(i){
+			case 1:
+				estado_juego = verificar_gano(tablero, 'X');
+				break;
+			case 2:
+				estado_juego = verificar_gano(tablero, 'O');
+		}
+		if(estado_juego == TERMINO){
+			i = 3;
+		}
+	}*/
+
+	return estado_juego;
+}
+
 int main(){
 
 	char tablero[FILA][COLUMNA] = {};
+
 	int fila_ingresada = -1;
 	int columna_ingresada = -1;
 	char signo = '-';
@@ -31,7 +140,7 @@ int main(){
 
 		tablero[fila_ingresada][columna_ingresada] = signo;
 
-
+		//muestra el tablero
 		for(int fila_actual = 0; fila_actual < FILA ; fila_actual ++ ){
 
 			for (int columna_actual = 0; columna_actual < COLUMNA; columna_actual++){
@@ -42,21 +151,7 @@ int main(){
 			cout<<endl<<endl<<endl;
 		}
 
-		//verica si gano el jugador X en las lineas horizontales
-		for(int fila_actual = 0; fila_actual < FILA ; fila_actual ++ ){
-			if((tablero[fila_actual][0] =='X') && (tablero[fila_actual][1] =='X') && (tablero[fila_actual][2] =='X')){
-				cout<<"Gano el jugador X"<<endl;
-				estado_juego = TERMINO;
-			}
-		}
-
-		//verica si gano el jugador O en las lineas horizontales
-		for(int fila_actual = 0; fila_actual < FILA ; fila_actual ++ ){
-			if((tablero[fila_actual][0] =='O') && (tablero[fila_actual][1] =='O') && (tablero[fila_actual][2] =='O')){
-				cout<<"Gano el jugador O"<<endl;
-				estado_juego = TERMINO;
-			}
-		}
+		estado_juego = verificar_gano_jugador(tablero);		
 
 
 	}while(estado_juego != TERMINO);
