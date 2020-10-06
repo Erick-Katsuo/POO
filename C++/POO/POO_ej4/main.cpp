c36d596d35799128ced7a8d2d10717ee4aa38ae4
Katsuo <49467285+Erick-Katsuo@users.noreply.github.com>
2020-10-06 11:17:18 -0300

    Add files via upload

--- C++/POO/POO_ej4/main.cpp
+++ C++/POO/POO_ej4/main.cpp
@@ -4,14 +4,9 @@ using namespace std;
 
 #include "Usuario.hpp"
 
+const int MAX_REGISTRO = 3;
 
-
-
-int main(){
-	Usuario registro[3];
-
-	string usuario_buscado = "def usuario buscado";
-
+void cargar_informacion(Usuario registro[]){
 	registro[0].asignar_nombre("Erick");
 	registro[0].asignar_contrasenia("123");
 	registro[0].asignar_nivel_acceso("Vendedor");
@@ -26,28 +21,59 @@ int main(){
 	registro[2].asignar_contrasenia("123asfd");
 	registro[2].asignar_nivel_acceso("administrador");
 	registro[2].asignar_saldo(200.0F);
+}
 
-	/*for(int i = 0; i < 3 ; i++){
+
+void mostrar_usuarios(Usuario registro[]){
+	for(int i = 0; i < MAX_REGISTRO ; i++){
 		cout << registro[i].obtener_nombre() << endl;
 		cout << registro[i].obtener_contrasenia() << endl;
 		cout << registro[i].obtener_nivel_acceso() << endl;
 		cout << registro[i].obtener_saldo() << endl << endl;
-	}*/
+	}
+}
 
+void verificar_contrasenia(Usuario usuario_encontrado, int i){
+	string contrasenia_ingr = "def contrasenia";
+	cout<<"Ingrese contrasenia"<<endl;
+	cin>> contrasenia_ingr;
+	if( usuario_encontrado.obtener_contrasenia() == contrasenia_ingr  ){
+		cout<<"Contrasenia aceptada"<<endl;
+	}else{
+		cout<<"Contrasenia o usuario no corresponden"<<endl;
+	}	
+}
+
+void buscar_usuario(Usuario registro[]){
+	string usuario_buscado = "def usuario buscado";
 	cout<<"Que usuario desea buscar?"<<endl;
 	cin >> usuario_buscado;
 
-	for(int i = 0; i < 3 ; i++){
+	for(int i = 0; i < MAX_REGISTRO ; i++){
 		if(registro[ i ].obtener_nombre() == usuario_buscado ){
 			cout<<"Encontro usuario!"<<endl;
 			//verificar contrasenia
-			i = 3;
+			verificar_contrasenia( registro[i] , i);
+			i = MAX_REGISTRO;
 		}else{
 			cout<<"No existe tal usuario"<<endl;
 		}
 	}
+}
 
 
+int main(){
+	Usuario registro[MAX_REGISTRO];
+
+	//actualiza la informacion del registro
+	cargar_informacion(registro);
+
+	//muestra todos los usuarios
+	mostrar_usuarios(registro);
+
+	//busca un usuario
+	buscar_usuario(registro);
+	
 
 	return 0;
 }
